268a373a6a716bc1d696afe52255411741d245ce
Natali2707 <105042840+Natali2707@users.noreply.github.com>
2023-04-05 23:21:09 +0300

    Updates

---
+++ precache-manifest.7059f50ff32216cae3ac46b863fe40ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07c729f51f16e3fa871d",
+    "url": "/react-tips-calculator/static/js/main.07c729f5.chunk.js"
+  },
+  {
+    "revision": "b2f264b0eecd3e9dadd4",
+    "url": "/react-tips-calculator/static/js/1.b2f264b0.chunk.js"
+  },
+  {
+    "revision": "2134981c078b5a0d9efe",
+    "url": "/react-tips-calculator/static/js/runtime~main.2134981c.js"
+  },
+  {
+    "revision": "df9a5ef422f1a003af9455557ca621f9",
+    "url": "/react-tips-calculator/static/media/shape.df9a5ef4.svg"
+  },
+  {
+    "revision": "33fd335b4c1b979eb208663a98fc72a3",
+    "url": "/react-tips-calculator/index.html"
+  }
+];
